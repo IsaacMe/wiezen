af14c8229207b5e8622bf974db402209a4b964ed
Isaac Meers <imeers@me.com>
2026-01-24 23:39:02 +0100

    Fix issue with listening to changes in function and cache

--- src/app/score-current-table/score-current-table.component.ts
+++ src/app/score-current-table/score-current-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PlayerService } from '../player.service';
 import { GameService } from '../game.service';
+import { Subscription } from 'rxjs';
 import Big from 'big.js';
 
 @Component({
@@ -9,26 +10,37 @@ import Big from 'big.js';
     styleUrls: ['./score-current-table.component.css'],
     standalone: false
 })
-export class ScoreCurrentTableComponent implements OnInit {
+export class ScoreCurrentTableComponent implements OnInit, OnDestroy {
+  totalScores: Big[] = [];
+  lastScores: Big[] = [];
+  round: number = 0;
+  currentGiver: number = 0;
+
+  private subscription: Subscription | null = null;
 
   constructor(public players: PlayerService, private game: GameService) { }
 
   ngOnInit() {
+    this.updateScores();
+    this.subscription = this.game.scoreTable.getObservableSize().subscribe(() => {
+      this.updateScores();
+    });
   }
 
-  getTotalScores(): Big[] {
-    return this.game.scoreTable.getTotals();
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
   }
 
-  getLastScores(): Big[] {
-    return this.game.scoreTable.getLastScores();
-  }
+  private updateScores(): void {
+    this.round = this.game.scoreTable.getRoundCount();
+    this.currentGiver = (this.round - 1) % 4;
 
-  getRound(): number {
-    return this.game.scoreTable.getRoundCount();
-  }
-
-  getCurrentGiver(): number {
-    return (this.getRound() - 1) % 4;
+    if (this.round > 0) {
+      this.totalScores = this.game.scoreTable.getTotals();
+      this.lastScores = this.game.scoreTable.getLastScores();
+    } else {
+      this.totalScores = [];
+      this.lastScores = [];
+    }
   }
 }
